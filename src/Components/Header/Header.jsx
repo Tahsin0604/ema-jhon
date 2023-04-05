abc2f6c207a4c8127da6a44dadff5b2997827c5a
Tahsin0604 <natikalamtahsin@gmail.com>
2023-04-05 16:50:26 +0600

    router

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Logo from '../../assets/images/Logo.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className='flex justify-between items-center bg-[#1C2B35] px-28 py-4'>
       <img src={Logo} alt="" />
       <div className='flex justify-center items-center lg:gap-12 md:gap-6 gap-4 text-lg text-white'>
-        <a href="" className='hover:text-orange-300'>Order</a>
-        <a href="" className='hover:text-orange-300'>Order Review</a>
-        <a href="" className='hover:text-orange-300'>Manage Inventory</a>
-        <a href="" className='hover:text-orange-300'>Login</a>
+        <Link to="/" className='hover:text-orange-300'>Shop</Link>
+        <Link to="/orders" className='hover:text-orange-300'>Orders</Link>
+        <Link to="/inventory" className='hover:text-orange-300'>Inventory</Link>
+        <Link to="/login" className='hover:text-orange-300'>Login</Link>
       </div>
       
     </div>
---
+++ src/Components/Orders/Orders.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Orders = () => {
+  return (
+    <div>
+      <div className="flex">
+      <div className="grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 gap-4 w-4/5 px-16 py-6">
+        
+      </div>
+      <Cart cart={[]}></Cart>
+    </div>
+    </div>
+  );
+};
+
+export default Orders;
